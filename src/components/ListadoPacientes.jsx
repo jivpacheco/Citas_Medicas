1d3f4475498a1cd7362e0538a609054d65d25fd8
Jose Valencia <jivpacheco@hotmail.com>
2023-01-04 22:43:56 -0300

    configurando Listado Pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,19 @@
-function ListadoPacientes() {
+import Paciente from "./Paciente"
+
+
+const ListadoPacientes = () => {
   return (
-    <div className="w-1/2 lg:w-3/5">
-      <h1>Listado de pacientes</h1>
+    <div className="md:w-1/2 lg:w-3/5  md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Administra tus {''}
+        <span className="text-indigo-600 font-bold" > Pacientes y citas </span>
+      </p>
+      {/*nos llevamos el detalle de paciente como un componente paciente.js para poderlo replicar*/}
+      <Paciente />
+      <Paciente />
+      <Paciente />
+      
     </div>
   )
 }
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,30 @@
+// aqui colocamos el codigo que se detalla en listado paciente
+const Paciente = () => {
+    return (
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+                <span className="font-normal normal-case">Ignacio</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+                <span className="font-normal normal-case">correo@correo.cl</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+                <span className="font-normal normal-case">05/01/2023</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
+                <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae aliquam harum autem labore nam. Animi quam reiciendis exercitationem accusamus rem similique magni dignissimos dolores earum, consequatur in temporibus nihil et.</span>
+            </p>
+
+        </div>
+    )
+}
+
+export default Paciente
